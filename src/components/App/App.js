cea44d811c91f404432c0b6e08f02284cfe9adff
Jeo D <e8gea6w5z8id@opayq.com>
2019-03-18 16:55:17 -0600

    Initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Palette Picker</h1>
+      </div>
+    );
+  }
+}
