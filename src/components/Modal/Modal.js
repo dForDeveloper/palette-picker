c606eb2ce565592b2233968c788f93909df30fe5
Jeo D <e8gea6w5z8id@opayq.com>
2019-03-19 14:30:27 -0600

    Add input fields and button to Modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 
 export class Modal extends Component {
+  handleSubmit = (event) => {
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <article className="Modal">
-          <h5>save palette</h5>
-        </article>
+      <form className="Modal" onSubmit={this.handleSubmit}>
+        <h4 className="Modal--h4">save palette</h4>
+        <input className="Modal--input" placeholder="project name"/>
+        <input className="Modal--input" placeholder="palette name"/>
+        <button className="Modal--button">
+          save
+        </button>
+      </form>
     );
   }
 }
