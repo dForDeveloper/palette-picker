c364b05c49a0f242899c61bdd8ebc46ea22b6739
Jeo D <e8gea6w5z8id@opayq.com>
2019-03-18 20:57:54 -0600

    Add feature to lock colors, resolve #1

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,7 +4,8 @@ export class App extends Component {
   constructor() {
     super();
     this.state = {
-      colors: []
+      colors: [],
+      lockedColors: []
     };
   }
 
@@ -13,29 +14,56 @@ export class App extends Component {
   }
 
   generateColors = () => {
-    const colors = []
+    const { colors, lockedColors } = this.state;
+    const newColors = [];
     for (let i = 0; i < 5; i++) {
-      let color = '#';
-      for (let j = 0; j < 3; j++) {
-        const num = Math.floor(Math.random() * 256);
-        color += num < 16 ? `0${num.toString(16)}` : num.toString(16);
+      if (lockedColors.includes(i)) {
+        newColors.push(colors[i]);
+      } else {
+        let newColor = '#';
+        for (let j = 0; j < 3; j++) {
+          const num = Math.floor(Math.random() * 256);
+          newColor += num < 16 ? `0${num.toString(16)}` : num.toString(16);
+        }
+        newColors.push(newColor);
       }
-      colors.push(color);
     }
-    this.setState({ colors });
+    this.setState({ colors: newColors });
   }
 
   renderColors = () => {
-    return this.state.colors.map(color => {
+    const { colors, lockedColors } = this.state; 
+    return colors.map((color, index) => {
+      const locked = lockedColors.includes(index);
+      const red = parseInt(color.slice(1, 3), 16);
+      const green = parseInt(color.slice(3, 5), 16);
+      const blue = parseInt(color.slice(5), 16);
+      const luma = 0.2126 * red + 0.7152 * green + 0.0722 * blue;
       return (
         <section
-          className="main--color-section"
+          id={color}
+          className={`section--locked-${locked}-${luma < 128}`}
           key={color}
           style={{ backgroundColor: color }}
+          onClick={this.handleClick}
         >
+          <h3 className={`h3--dark-${luma < 128}`}>{color}</h3>
         </section>
       );
     });
+  }
+
+  handleClick = (event) => {
+    let newLockedColors = [];
+    const { id } = event.target;
+    const { colors, lockedColors } = this.state; 
+    const clickedIndex = colors.findIndex(color => color === id);
+    if (lockedColors.includes(clickedIndex)) {
+      newLockedColors = lockedColors.filter(index => index !== clickedIndex);
+    } else {
+      newLockedColors = [...lockedColors, clickedIndex];
+    }
+    this.setState({ lockedColors: newLockedColors });
   }
 
   render() {
