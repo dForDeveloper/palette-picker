0ff6e76a9c8abe272026ba548ddb78771be87b80
Jeo D <e8gea6w5z8id@opayq.com>
2019-03-19 22:17:45 -0600

    Add loading, projects, and error actions/reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,3 +7,18 @@ export const setLockedColors = (lockedColors) => ({
   type: 'SET_LOCKED_COLORS',
   lockedColors
 });
+
+export const setProjects = (projects) => ({
+  type: 'SET_PROJECTS',
+  projects
+});
+
+export const toggleLoading = (bool) => ({
+  type: 'TOGGLE_LOADING',
+  bool
+});
+
+export const setError = (message) => ({
+  type: 'SET_ERROR',
+  message
+});
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,14 @@
 import { combineReducers } from 'redux';
 import { colorsReducer } from './colorsReducer';
 import { lockedColorsReducer } from './lockedColorsReducer';
+import { projectsReducer } from './projectsReducer';
+import { loadingReducer } from './loadingReducer';
+import { errorReducer } from './errorReducer';
 
 export const rootReducer = combineReducers({
   colors: colorsReducer,
-  lockedColors: lockedColorsReducer
+  lockedColors: lockedColorsReducer,
+  projects: projectsReducer,
+  loading: loadingReducer,
+  error: errorReducer
 });
