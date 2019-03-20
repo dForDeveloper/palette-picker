493c252ad9448e81ab611702856a97e143960b1b
Jeo D <e8gea6w5z8id@opayq.com>
2019-03-19 22:19:50 -0600

    Overhaul Modal component

---
+++ src/containers/Modal/Modal.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import { saveNewPalette } from '../../thunks/saveNewPalette';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+export class Modal extends Component {
+  constructor() {
+    super();
+    this.state = {
+      projectName: '',
+      paletteName: ''
+    }
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    const { projectName, paletteName } = this.state;
+    const { projects, colors, saveNewPalette } = this.props;
+    await saveNewPalette(projectName, paletteName, projects, colors);
+    this.props.toggleModal();
+  }
+
+  render() {
+    return (
+      <form className="Modal" onSubmit={this.handleSubmit}>
+        <h4 className="Modal--h4">save palette</h4>
+        <input
+          className="Modal--input"
+          name="projectName"
+          placeholder="project name"
+          value={this.state.projectName}
+          onChange={this.handleChange}
+          required 
+        />
+        <input
+          className="Modal--input"
+          name="paletteName"
+          placeholder="palette name"
+          value={this.state.paletteName}
+          onChange={this.handleChange}
+          required
+        />
+        <button className="Modal--button">
+          save
+        </button>
+      </form>
+    );
+  }
+}
+
+export const mapStateToProps = (state) => ({
+  colors: state.colors,
+  projects: state.projects
+});
+
+export const mapDispatchToProps = (dispatch) => ({
+  saveNewPalette: (projectName, paletteName, projects, colors) => {
+    dispatch(saveNewPalette(projectName, paletteName, projects, colors));
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Modal);
+
+Modal.propTypes = {
+  colors: PropTypes.array,
+  projects: PropTypes.array,
+  saveNewPalette: PropTypes.func,
+  toggleModal: PropTypes.func
+}
